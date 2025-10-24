87c373bcad18936397887348170f92726f58303c
ARMANSYAH112 <ikanchana617@gmail.com>
2025-10-24 23:25:23 +0700

    Update index.lua

--- index.lua
+++ index.lua
@@ -1,5 +1,16 @@
 local Luna = loadstring(game:HttpGet("https://raw.githubusercontent.com/Nebula-Softworks/Luna-Interface-Suite/refs/heads/main/source.lua", true))()
 
+-- Deteksi key dari file sebelum window dibuat
+_G.CurrentKey = ""
+local KeyFile = "XentaurHub/key.txt"
+
+if isfile(KeyFile) then
+	local SavedKey = readfile(KeyFile)
+	if SavedKey == "ok" or SavedKey == "ArmansyahKeceAnjir" then
+		_G.CurrentKey = SavedKey
+	end
+end
+
 local Window = Luna:CreateWindow({
 	Name = "XentaurHub",
 	Subtitle = nil,
@@ -28,19 +39,10 @@ local Window = Luna:CreateWindow({
 	}
 })
 
--- Deteksi key yang dipakai
-_G.CurrentKey = ""
-for _, key in ipairs({"ok", "ArmansyahKeceAnjir"}) do
-	local KeyFolder = (Window.KeySettings.SaveInRoot and ".Rayfield") or "Rayfield"
-	local KeyFile = KeyFolder .. "/" .. Window.KeySettings.FileName .. ".txt"
-	
-	if isfile(KeyFile) then
-		local SavedKey = readfile(KeyFile)
-		if SavedKey == key then
-			_G.CurrentKey = key
-			break
-		end
-	end
+-- Update CurrentKey setelah key berhasil divalidasi
+if isfile(KeyFile) then
+	local SavedKey = readfile(KeyFile)
+	_G.CurrentKey = SavedKey
 end
 
 -- Load file utama setelah key benar
